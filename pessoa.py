e8fef0a7114a338b2e5caaf3bf36d8b1621904e2
Thiago Adam <thiagohsm2009@hotmail.com>
2023-04-21 22:06:51 -0300

    mudados o tema para concessionaria

--- pessoa.py
+++ pessoa.py
@@ -1,6 +1,5 @@
 class Pessoa:
-    def __init__(self, nome, cpf, idade, endereco):
+    def __init__(self, nome, endereco, numero_de_telefone):
         self.nome = nome
-        self.cpf = cpf
-        self.idade = idade
         self.endereco = endereco
+        self.numero_de_telefone = numero_de_telefone
