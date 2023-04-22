63caf06de014f489d786b0835aebf269a6e7f1a0
Thiago Adam <thiagohsm2009@hotmail.com>
2023-04-21 23:32:49 -0300

    fiz algumas pequenas mudancas

--- pessoa_fisica.py
+++ pessoa_fisica.py
@@ -2,7 +2,9 @@ from pessoa import Pessoa
 
 class PessoaFisica(Pessoa):
 
-    def __init__(self, nome, endereco, numero_de_telefone, cpf, idade, ):
+    def __init__(self, nome, endereco, numero_de_telefone, cpf, idade):
         super().__init__(nome,  endereco, numero_de_telefone)
         self._cpf = cpf
         self._idade = idade
+
+        #print(isinstance(p1, Pessoa), isinstance(pf1, PessoaFisica))  testar hierarquia
