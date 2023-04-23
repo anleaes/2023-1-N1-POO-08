6d90d0da1a427c6266db8bc86ec53f6741ae2d26
Matheus Protti <prottimatheus@gmail.com>
2023-04-23 16:53:28 -0300

    Adicionando arquivos

--- carro.py
+++ carro.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-from veiculo import Veiculo
-
-class Carro(Veiculo):
-    def __init__(self, marca, modelo, ano, preco, cor, placa):
-        super().__init__(marca, modelo, ano, preco)
-        self.cor = cor
-        self.placa = placa
-=======
 class Carro:
     def __init__(self, marca, ano, tipo):
         self.marca = marca
         self.ano = ano
         self.tipo = tipo
->>>>>>> d9d0dc94a7284e7cac2c9c2f98a7cc7fb7cd2b8d
