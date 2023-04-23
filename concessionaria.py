8b1e2e986192b8376bdb9f28cb3c348fbfeb224f
Thiago Adam <thiagohsm2009@hotmail.com>
2023-04-23 15:52:07 -0300

    teste

--- concessionaria.py
+++ concessionaria.py
@@ -1,9 +1,10 @@
 class Concessionaria:
     
-    def __init__(self, endereco, cnpj, numero_da_concessionaria, carro, moto, pessoa):
+    def __init__(self, endereco, cnpj, carro, moto, pessoa):
         self._endereco = endereco
         self._cnpj = cnpj
-        self._numero_da_concessionaria = numero_da_concessionaria
         self._carro = carro
-        self._moto - moto
+        self._moto = moto
         self._pessoa = pessoa
+
+        #######
